e5a3f8c6f9a0ac3a4d02988e8afd81baf4602c8a
cristiielciu <cristian.ielciu@takeofflabs.com>
2023-05-04 16:13:37 +0300

    Documentation for UserInfo

--- lib/oauth/user_info.rb
+++ lib/oauth/user_info.rb
@@ -15,7 +15,15 @@ module Oauth
   #
   class UserInfo
     #
+    # The info method makes a request to an OpenID Connect provider's userinfo
+    # endpoint to retrieve information about the user associated with the
+    # provided access token.
     #
+    # @param [String] token - the access token provided by the IP application
+    #
+    # If the response from the userinfo endpoint has a status code of 200,
+    # the method returns a parsed JSON object representing the user information.
+    # Otherwise, the method returns nil.
     #
     def self.info(token)
       response = HTTParty.get(
@@ -26,6 +34,18 @@ module Oauth
       return JSON.parse(response.body) if response.code == 200
     end
 
+    #
+    # This get_new_token_and_refresh method obtains a new access token and refresh
+    # token pair from IP using the user's existing refresh token.
+    #
+    # @param [Object] user - object representing the user whose access token needs
+    # to be refreshed
+    #
+    # If the request is successful, the method returns a hash containing two keys:
+    # user_token and user_refresh_token. The value of the user_token key is the
+    # new access token obtained from the IP, while the value of the user_refresh_token
+    # key is the new refresh token obtained from the OIDC provider.
+    #
     def self.get_new_token_and_refresh(user)
       response = HTTParty.post(
         ::OpenIdConnectClient.discover.token_endpoint,
@@ -41,7 +61,20 @@ module Oauth
     end
 
     #
+    # The find_and_update_or_create_user method finds or creates a user record in the
+    # local application database based on information obtained from the IP.
     #
+    # @param [Hash] token_and_refresh - hash thant contains token and refresh token
+    # @param [Object] user - object representing the user
+    #
+    # If a user object is provided and token_and_refresh is not, the method will attempt
+    # to retrieve the user's access token and refresh token from the user object and
+    # populate the token_and_refresh parameter accordingly. This assumes that the user
+    # object has already been authenticated with the IP and has valid tokens
+    # associated with it.
+    #
+    # The method returns the updated or newly created user object, or nil if no user object
+    # is found or created.
     #
     def self.find_and_update_or_create_user(token_and_refresh: {}, user: nil)
       if user.present? && token_and_refresh.blank?
@@ -64,6 +97,18 @@ module Oauth
     end
   end
 
+  #
+  # The update_existing_user method updates an existing user record in the local application
+  # database with the latest token and refresh token information obtained from an IP and
+  # user information associated with the user's IP account, keeping the local application's
+  # user records up-to-date with the latest information from an IP.
+  #
+  # @param [Object] user - object representing the user
+  # @param [Hash] token_and_refresh - hash thant contains token and refresh token
+  # @param [Hash] user_info - hash thant contains all user information from IP
+  #
+  # This method returns updated user record
+  #
   def self.update_existing_user(user, token_and_refresh, user_info)
     user.assign_attributes(user_attributes(user_info, token_and_refresh))
     user.save!
@@ -71,6 +116,17 @@ module Oauth
     user
   end
 
+  #
+  # The user_create_or_update_with_associations method creates or updates a user record in the 
+  # local application database with the latest token and refresh token information obtained
+  # from an IP and user information associated with the user's IP account.
+  #
+  # @param [Object] user - object representing the user
+  # @param [Hash] token_and_refresh - hash thant contains token and refresh token
+  # @param [Hash] user_info - hash thant contains all user information from IP
+  #
+  # This method returns updated user record
+  #
   def self.user_create_or_update_with_associations(user, token_and_refresh, user_info)
     return if user_info.blank?
 
@@ -83,6 +139,18 @@ module Oauth
     user&.reload
   end
 
+  #
+  # The user_attributes method constructs a hash of attributes that can be used to
+  # create or update a user record in the local application database based on the
+  # user information and token and refresh token information obtained from IP.
+  #
+  # @param [Hash] user_info - hash thant contains all user information from IP
+  # @param [Hash] token_and_refresh - hash thant contains token and refresh token
+  #
+  # This method returns a hash, which contains the values of all the attributes
+  # needed to create or update a user record in the local application database
+  # based on the latest information obtained from the IP.
+  #
   def self.user_attributes(user_info, token_and_refresh)
     attrs = {
       user.send(GgIpClientMate::Config.oauth_token_attribute_name) => token_and_refresh[:user_token],
